1882c64a998102136e57cc4850967cdca775ae58
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2022-04-06 23:05:53 -0300

    rota para filtrar por data adicionado

--- routes/fixedExpenses.route.go
+++ routes/fixedExpenses.route.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"context"
 	"encoding/json"
 	db2 "financasApi/db"
 	"io/ioutil"
@@ -11,12 +12,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var ctx context.Context
+
 type FixedExpenses struct {
 	Idfixed      int     `json:"id"`
 	NameExpense  string  `json:"name"`
 	ValueExpense float64 `json:"value"`
 	DueDate      string  `json:"duedate"`
 	PayDate      string  `json:"paydate"`
+	DateCurrent  string  `json:"datecurrent"`
 }
 
 type ResponseErr struct {
@@ -35,7 +39,7 @@ func listFixedExpenses(w http.ResponseWriter, r *http.Request) {
 	var fixedExpenses []FixedExpenses = make([]FixedExpenses, 0)
 	for registers.Next() {
 		var fixedExpense FixedExpenses
-		errScan := registers.Scan(&fixedExpense.Idfixed, &fixedExpense.NameExpense, &fixedExpense.ValueExpense, &fixedExpense.DueDate, &fixedExpense.PayDate)
+		errScan := registers.Scan(&fixedExpense.Idfixed, &fixedExpense.NameExpense, &fixedExpense.ValueExpense, &fixedExpense.DueDate, &fixedExpense.PayDate, &fixedExpense.DateCurrent)
 		if errScan != nil {
 			log.Println("FixedExepenses: errScan: " + errScan.Error())
 			continue
@@ -82,7 +86,7 @@ func addFixedExpense(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// insert in database
-	result, errInsert := db2.Db.Exec("INSERT INTO fixed_expenses (name_expense,value_expense,due_date,pay_date) VALUES (?,?,?,?)", newFixedExpense.NameExpense, newFixedExpense.ValueExpense, newFixedExpense.DueDate, newFixedExpense.PayDate)
+	result, errInsert := db2.Db.Exec("INSERT INTO fixed_expenses (name_expense,value_expense,due_date,pay_date, date_current) VALUES (?,?,?,?,?)", newFixedExpense.NameExpense, newFixedExpense.ValueExpense, newFixedExpense.DueDate, newFixedExpense.PayDate, newFixedExpense.DateCurrent)
 
 	idGenerated, errLastInsertId := result.LastInsertId()
 
@@ -124,14 +128,14 @@ func alterFixedExpense(w http.ResponseWriter, r *http.Request) {
 
 	register := db2.Db.QueryRow("SELECT idfixed, name_expense, value_expense, due_date, pay_date FROM fixed_expenses WHERE idfixed = ?", id)
 	var fixedExpense FixedExpenses
-	errScan := register.Scan(&fixedExpense.Idfixed, &fixedExpense.NameExpense, &fixedExpense.ValueExpense, &fixedExpense.DueDate, &fixedExpense.PayDate)
+	errScan := register.Scan(&fixedExpense.Idfixed, &fixedExpense.NameExpense, &fixedExpense.ValueExpense, &fixedExpense.DueDate, &fixedExpense.PayDate, &fixedExpense.DateCurrent)
 
 	if errScan != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	_, errExec := db2.Db.Exec("UPDATE fixed_expenses SET name_expense = ?, value_expense = ?, due_date = ?, pay_date = ? WHERE idfixed = ?", alterFixed.NameExpense, alterFixed.ValueExpense, alterFixed.DueDate, alterFixed.PayDate, id)
+	_, errExec := db2.Db.Exec("UPDATE fixed_expenses SET name_expense = ?, value_expense = ?, due_date = ?, pay_date = ?, date_current = ? WHERE idfixed = ?", alterFixed.NameExpense, alterFixed.ValueExpense, alterFixed.DueDate, alterFixed.PayDate, alterFixed.DateCurrent, id)
 
 	if errExec != nil {
 		log.Println("AlterFixedExpense: errExec: " + errExec.Error())
@@ -170,6 +174,57 @@ func deleteFixed(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
+func filterDate(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["datecurrent"])
+	// id := vars["datecurrent"]
+
+	registers, errSelect := db2.Db.Query("SELECT * FROM fixed_expenses WHERE date_current = ?", id)
+
+	if errSelect != nil {
+		log.Println("fixed_expenses: " + errSelect.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer registers.Close()
+	var fixedExpenses []FixedExpenses = make([]FixedExpenses, 0)
+
+	for registers.Next() {
+		var fixedExpense FixedExpenses
+		errScan := registers.Scan(&fixedExpense.Idfixed, &fixedExpense.NameExpense, &fixedExpense.ValueExpense, &fixedExpense.DueDate, &fixedExpense.PayDate, &fixedExpense.DateCurrent)
+		if errScan != nil {
+			log.Println("FixedExepenses: errScan: " + errScan.Error())
+			continue
+		}
+
+		fixedExpenses = append(fixedExpenses, fixedExpense)
+	}
+
+	if len(fixedExpenses) == 0 {
+		log.Println("Fixed expenses empty")
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	errCloseRegisters := registers.Close()
+
+	if errCloseRegisters != nil {
+		log.Println("filter date: errCloseRegisters: " + errCloseRegisters.Error())
+	}
+
+	// errScan := register.Scan(&fixedExpense.Idfixed, &fixedExpense.NameExpense, &fixedExpense.ValueExpense, &fixedExpense.DueDate, &fixedExpense.PayDate, &fixedExpense.DateCurrent)
+
+	// if errScan != nil {
+	// 	log.Println("filterDate: errScan: " + errScan.Error())
+	// 	w.WriteHeader(http.StatusNotFound)
+	// 	return
+	// }
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(fixedExpenses)
+}
+
 // func test(w http.ResponseWriter, r *http.Request) {
 // 	fmt.Fprint(w, "teste")
 // }
@@ -180,4 +235,5 @@ func ConfigRoute(router *mux.Router) {
 	router.HandleFunc("/addfixedexpense", addFixedExpense).Methods("POST")
 	router.HandleFunc("/fixedexpenses/{idfixed}", alterFixedExpense).Methods("PUT")
 	router.HandleFunc("/fixedexpenses/{idfixed}", deleteFixed).Methods("DELETE")
+	router.HandleFunc("/fixedexpensesdate/{datecurrent}", filterDate).Methods("GET")
 }
